95b2d30152024d010e2bc574dc2330e227133e50
zahrank21 <zahrank@gmail.com>
2018-01-29 00:46:59 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,6 +1,6 @@
 ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   def alphabetize(arr)
-
+    arr.sort_by! do |a|
      positions = []
      a.each_char do |char|
        positions << ESPERANTO_ALPHABET.index(char)
