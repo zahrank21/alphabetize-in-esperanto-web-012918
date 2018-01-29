cabacd92dcc0ac2cb9cf6663b97081b8bcf543ef
zahrank21 <zahrank@gmail.com>
2018-01-29 00:49:48 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,9 +1,9 @@
-ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   def alphabetize(arr)
     arr.sort_by! do |a|
      sub_array = []
      a.each_char do |el|
-       sub_array << ESPERANTO_ALPHABET.index(el)
+       sub_array << alphabet.index(el)
      end
      sub_array
    end
